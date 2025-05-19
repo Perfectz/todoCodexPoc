60c34c9cb6953904c0a46793e30ec38c7d1e9ab1
Patrick Zgambo <pzgambo@gmail.com>
2025-05-19 15:16:03 -0400

    fix: restore x namespace in style dictionaries

--- src/TaskTracker.Mobile/Views/MainPage.xaml.cs
+++ src/TaskTracker.Mobile/Views/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 using Microsoft.Maui.Controls;
+using System.Threading.Tasks;
 using TaskTracker.Mobile.ViewModels;
 using TodoCodexPoc.Models;
 
@@ -10,6 +11,7 @@ public partial class MainPage : ContentPage
     {
         InitializeComponent();
         BindingContext = viewModel;
+        TaskList.ItemAppearing += OnItemAppearing;
     }
 
     private void OnCheckBoxChanged(object sender, CheckedChangedEventArgs e)
@@ -17,6 +19,18 @@ public partial class MainPage : ContentPage
         if (sender is CheckBox cb && cb.BindingContext is TodoItem item && BindingContext is MainPageViewModel vm)
         {
             vm.ToggleDoneCommand.Execute(item);
+        }
+    }
+
+    private async void OnItemAppearing(object? sender, ItemVisibilityEventArgs e)
+    {
+        if (e.Item is ViewCell v)
+        {
+            v.Opacity = 0;
+            v.TranslationX = 20;
+            await Task.WhenAll(
+                v.FadeTo(1, 200, Easing.CubicOut),
+                v.TranslateTo(0, 0, 200, Easing.CubicOut));
         }
     }
 }
