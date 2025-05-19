121a8943aadf216b7c60bc8b12b27b4720f815a1
Patrick Zgambo <pzgambo@gmail.com>
2025-05-19 15:02:16 -0400

    feat(ui): bottom tab bar scaffold

--- src/TaskTracker.Mobile/App.xaml.cs
+++ src/TaskTracker.Mobile/App.xaml.cs
@@ -8,6 +8,6 @@ public partial class App : Application
     public App(MainPage page)
     {
         InitializeComponent();
-        MainPage = new NavigationPage(page);
+        MainPage = new AppShell();
     }
 }
