e3dea331db68d5f683fab36f367f7ab18ef8543f
Patrick Zgambo <pzgambo@gmail.com>
2025-05-19 11:14:17 -0400

    Add basic .NET MAUI Android project

---
+++ TodoMauiApp/Platforms/Android/MainApplication.cs
@@ -0,0 +1,16 @@
+using Microsoft.Maui;
+using Android.App;
+using Android.Runtime;
+
+namespace TodoMauiApp;
+
+[Application]
+public class MainApplication : MauiApplication
+{
+    public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+        : base(handle, ownership)
+    {
+    }
+
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
